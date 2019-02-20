bdb1c7d5c10d8d562d68a2ba000079c807cb28b0
vick3d <viktoro0090@gmail.com>
2019-02-20 01:34:34 +0100

    adds buttons for rock paper scissors

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -1,16 +1,17 @@
-const { Given, When, Then } = require("cucumber");
- 
- Given('I visit the site', async function () {
-    return await this.openHomePage()
-  });
+const { Given, When, Then, After } = require("cucumber");
 
-  When('I click on {string}', function (string) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
+	After(async function() {
+			return await this.closeHomePage()
+	})
 
+	Given('I visit the site', async function () {
+			return await this.openHomePage()
+	});
 
-  Then('I should see {string}', function (string) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
+	When('I click on {string}', async function (btnName) {
+			return await this.clickOnBtn(btnName)
+	});
+
+	Then('I should see {string}', async function (content) {
+			return await this.pageHasTextContent(content)	
+	});
--- features/support/world.js
+++ features/support/world.js
@@ -13,6 +13,39 @@ class RpsWorld {
     this.page = await this.browser.newPage()
     await this.page.goto(HOME_PAGE)
   }
+
+  async pageHasTextContent(expectedContent) {
+      const pageContent = await this.page.content()
+      const actualContent = pageContent.match(expectedContent)[0]
+
+      expect(actualContent).to.be.eq(expectedContent)
+	}
+	
+	async clickOnBtn(btnName) {
+		const btnSelector = this.btnSelectorFromName(btnName.toLowerCase())
+		await this.page.waitForSelector(btnSelector)
+		await this.page.click(btnSelector)
+	}
+
+	btnSelectorFromName(btnName) {
+    switch (btnName) {
+      case 'rock':
+        return '.rock'
+        break
+      case 'paper':
+				return '.paper'
+			case 'scissors':
+				return '.scissors'
+        break
+      default:
+        throw `${btnName} button is not defined`
+        break
+    }
+  }
+
+	async closeHomePage() {
+    await this.browser.close()
+  }
 }
 
 setWorldConstructor(RpsWorld)
